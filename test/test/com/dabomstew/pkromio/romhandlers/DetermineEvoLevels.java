7c6f6485243005b177deeda08ade943aa7489747
Enrico <enbergmann@web.de>
2025-08-31 10:28:18 +0200

    Build level up evo triplets for comparison

--- test/test/com/dabomstew/pkromio/romhandlers/DetermineEvoLevels.java
+++ test/test/com/dabomstew/pkromio/romhandlers/DetermineEvoLevels.java
@@ -28,13 +28,23 @@ public class DetermineEvoLevels extends RomHandlerTest{
         int[] levelUpBSTaverage = new int[100];
         int[] levelUpBSTmin = new int[100];
         int[] levelUpBSTmax = new int[100];
-        int latestLevelUpEvo = writeBSTlists(levelUpEvos, levelUpBSTaverage, levelUpBSTmin, levelUpBSTmax);
-        // TODO likely not good enough, get list of evo triples (BSTold, BSTnew, evo level) instead for all levelUpEvos and work with that somehow
+        List<int[]> fromBST_toBST_evoLevel = new ArrayList<>();
+        int latestLevelUpEvo = getLevelUpInformation(levelUpEvos, levelUpBSTaverage, levelUpBSTmin, levelUpBSTmax,
+                fromBST_toBST_evoLevel);
 
+        // TODO ChatGPT approach three sounds good
         System.out.println(romName + " - latest level up evolution: " + latestLevelUpEvo);
         System.out.println("minimum BST at level: " + Arrays.toString(levelUpBSTmin));
         System.out.println("average BST at level: " + Arrays.toString(levelUpBSTaverage));
         System.out.println("maximum BST at level: " + Arrays.toString(levelUpBSTmax));
+        for (int k = 0; k<fromBST_toBST_evoLevel.size(); k++) {
+            int[] row = fromBST_toBST_evoLevel.get(k);
+            System.out.print(Arrays.toString(row) + " ");
+            if ((k+1)%9==0) System.out.println();
+        }
+        System.out.println();
+        // TODO place pk in fromBST_toBST_Level list at best fitting fromBST entry index: then, go in both directions,
+        // TOOD and determine evo level (penalizing distance in list)
         for (Species pk : nonLevelUpEvos) {
             int evoLevelFirstStage = 0;
             if (pk.getEvolutionsFrom().get(0).getType().usesLevel()) { // evolve first if level up evo
@@ -43,12 +53,10 @@ public class DetermineEvoLevels extends RomHandlerTest{
                 pk = pk.getEvolutionsFrom().get(0).getTo();
                 System.out.println(pkOldName + " --> " + pk.getName() + " (level up evo at level " + evoLevelFirstStage + ")");
             }
-            int bstPk = pk.getHp() + pk.getAttack() + pk.getDefense() + pk.getSpatk() + pk.getSpdef()
-                    + pk.getSpeed() + pk.getSpecial();
+            int bstPk = getBST(pk);
             for (Evolution evo : pk.getEvolutionsFrom()) {
                 Species evoOfPk = evo.getTo();
-                int bstEvoOfPk = evoOfPk.getHp() + evoOfPk.getAttack() + evoOfPk.getDefense() + evoOfPk.getSpatk()
-                        + evoOfPk.getSpdef() + evoOfPk.getSpeed() + evoOfPk.getSpecial();
+                int bstEvoOfPk = getBST(evoOfPk);
                 boolean evoBeforeLatestLvl = false;
                 for (int m = evoLevelFirstStage; m < latestLevelUpEvo; m++) {
                     // TODO the following is the thing that has to be figured out
@@ -96,7 +104,8 @@ public class DetermineEvoLevels extends RomHandlerTest{
         }
     }
 
-    private int writeBSTlists(List<Species> levelUpEvos, int[] levelUpBSTaverage, int[] levelUpBSTmin, int[] levelUpBSTmax) {
+    private int getLevelUpInformation(List<Species> levelUpEvos, int[] levelUpBSTaverage, int[] levelUpBSTmin, int[] levelUpBSTmax,
+                                      List<int[]> fromBST_toBST_evoLevel) {
         int latestLevelUpEvo = 0;
 
         boolean atLeastOneEvoHappend = true; // first time, update bst arrays
@@ -109,21 +118,26 @@ public class DetermineEvoLevels extends RomHandlerTest{
                     Species pk = levelUpEvos.get(k);
                     if (!pk.getEvolutionsFrom().isEmpty() && // has evolution
                             pk.getEvolutionsFrom().get(0).getExtraInfo() <= j + 1) { // currently analyzed level (j+1) is at least evo level
-                        if (pk.getEvolutionsFrom().get(0).getExtraInfo() > latestLevelUpEvo) {
-                            latestLevelUpEvo = pk.getEvolutionsFrom().get(0).getExtraInfo();
+                        int evoLevel = pk.getEvolutionsFrom().get(0).getExtraInfo();
+                        if (evoLevel > latestLevelUpEvo) {
+                            latestLevelUpEvo = evoLevel;
                         }
-                        levelUpEvos.set(k, pk.getEvolutionsFrom().get(0).getTo());
+                        int pkBST = getBST(pk);
+                        Species evoOfPk = pk.getEvolutionsFrom().get(0).getTo();
+                        int evoOfPkBST = getBST(evoOfPk);
+                        fromBST_toBST_evoLevel.add(new int[] {pkBST, evoOfPkBST, evoLevel});
+                        levelUpEvos.set(k, evoOfPk);
                         evolutionHappened = true;
                         atLeastOneEvoHappend = true;
                     }
                 }
             }
+            fromBST_toBST_evoLevel.sort(Comparator.comparingInt(r -> r[0]));
             if (atLeastOneEvoHappend) { // will enter this for j==0 always
                 int sumBSTs = 0;
                 for (int l = 0; l < levelUpEvos.size(); l++) {
                     Species pk = levelUpEvos.get(l);
-                    int bst = pk.getHp() + pk.getAttack() + pk.getDefense() + pk.getSpatk() + pk.getSpdef()
-                            + pk.getSpeed() + pk.getSpecial();
+                    int bst = getBST(pk);
                     if (bst < levelUpBSTmin[j] || levelUpBSTmin[j] == 0) {
                         levelUpBSTmin[j] = bst;
                     }
@@ -141,6 +155,10 @@ public class DetermineEvoLevels extends RomHandlerTest{
             atLeastOneEvoHappend = false;
         }
         return latestLevelUpEvo;
+    }
+
+    private int getBST(Species pk) {
+        return pk.getHp() + pk.getAttack() + pk.getDefense() + pk.getSpatk() + pk.getSpdef() + pk.getSpeed() + pk.getSpecial();
     }
 }
 
